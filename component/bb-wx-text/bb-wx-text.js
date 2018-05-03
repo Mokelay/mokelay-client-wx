267e7afc81b77415f2615204d41613fb871e3a26
陈征 <carl.chen@chinaredstar.com>
2018-05-03 15:16:20 +0800

    bb-wx-text

--- component/bb-wx-text/bb-wx-text.js
+++ component/bb-wx-text/bb-wx-text.js
@@ -4,20 +4,23 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    innerText: {
+      type: String,
+      value: 'default value',
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    someData: {}
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    load:function(){}
   }
 })
