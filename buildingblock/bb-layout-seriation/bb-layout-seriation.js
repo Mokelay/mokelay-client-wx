79d3771b1567e503050a41e681e0db1c95961991
xiaobc <527710279@qq.com>
2018-08-30 10:08:36 +0800

    顺序布局优化，支持排版设置

--- buildingblock/bb-layout-seriation/bb-layout-seriation.js
+++ buildingblock/bb-layout-seriation/bb-layout-seriation.js
@@ -29,7 +29,8 @@ Component({
    */
   data: {
     itemClass: "db",
-    realContent:[]
+    realContent:[],
+    layouts:[]//用于模板渲染排版，均分，按比例分等
   },
   //此时不能setData
   created:function(){
@@ -41,6 +42,9 @@ Component({
       itemClass:t.data.horizontal ? "df" : "db",
       realContent:t.data.content
     });
+    //顺序布局排版设置
+    let _layout = t._buildLayout();
+
   },
   //页面准备结束，此时可以用select获取节点
   ready:function(){
@@ -50,6 +54,52 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    /**
+     * 构建顺序布局排版
+     */
+    _buildLayout:function(){
+      let t=this;
+      let result = [];
+      const content = t.data.realContent;
+      if(content&&content.length>0){
+        content.forEach((item,index)=>{
+          let _style = "";
+          if (!t.data.layoutType || t.data.layoutType === 'average') {
+            _style = "flex: 1; width: 1%;";//1%解决flex1 不均分的bug
+          } else if (t.data.layoutType === 'compact') {
+            _style = "";
+          } else if (t.data.layoutType === 'proportion') {
+            let _flex = 1;
+            if (typeof (t.data.distributeBlock[index]) === 'string') {
+              _flex = Number(t.data.distributeBlock[index] || 1);
+            } else if (typeof (t.data.distributeBlock[index]) === 'object') {
+              _flex = Number(t.data.distributeBlock[index].alias || 1);
+            }
+            _style = "flex: " + _flex+"; width: 1%;";//1%解决flex1 不均分的bug
+          } else if (t.data.layoutType === 'rightauto') {
+            //左边固定宽度，右边自动填充
+            if (index == 0) {
+              _style = "flex:initial;display:block;";
+            } else {
+              _style = "flex:1;display:block;width:1%;";
+            }
+          } else if (t.data.layoutType === 'leftauto') {
+            //左侧自动填充，右侧固定
+            if (index == content.length - 1) {
+              _style = "flex:initial;display:block;";
+            } else {
+              _style = "flex:1;display:block;width:1%;";
+            }
+          }
+          if (!t.data.horizontal) {
+            _style = "";
+          }
+          result.push(_style);
+        });
+      }
+      t.setData({
+        layouts: result
+      });
+    }
   }
 })
