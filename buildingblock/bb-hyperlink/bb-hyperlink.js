48d63b184f1f4c712a7c5bc471c2f9a4e637c524
xiaobc <527710279@qq.com>
2018-09-01 18:18:35 +0800

    新增 超链接 组件

---
+++ buildingblock/bb-hyperlink/bb-hyperlink.js
@@ -0,0 +1,33 @@
+// buildingblock/bb-hyperlink/bb-hyperlink.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    domStyle:{
+      type:String
+    },
+    // href地址
+    hrefValue: {
+      type: String
+    },
+    // 文字内容
+    contentValue: {
+      type: String
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
