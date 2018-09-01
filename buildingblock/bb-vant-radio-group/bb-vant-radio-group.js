e4e3be406adab87f5f28dec4cfe7a09592a60ba4
minjie.wu <minjie.wu@chinaredstar.com>
2018-09-01 16:31:37 +0800

    优化单选组件

--- buildingblock/bb-vant-radio-group/bb-vant-radio-group.js
+++ buildingblock/bb-vant-radio-group/bb-vant-radio-group.js
@@ -28,18 +28,7 @@ Component({
         }]
     */
     fields: {
-      type: Array,
-      value: [{
-        text: '选项1',
-        value: '1',
-        subDescription: "默认值1",
-        disabled: false
-      }, {
-        text: '选项2',
-        value: '2',
-        subDescription: "默认值2",
-        disabled: false
-      }]
+      type: Array
     },
     /*选项数据 动态
         通过DS接口获取
@@ -57,18 +46,56 @@ Component({
       }
     }
   },
-
   /**
    * 组件的初始数据
    */
   data: {
-
+    valueBase:false,
+    realFields: [{
+      text: '选项1',
+      value: '1',
+      subDescription: "默认值1",
+      disabled: false,
+      checked:false
+    }, {
+      text: '选项2',
+      value: '2',
+      subDescription: "默认值2",
+      disabled: false,
+      checked:false
+    }]
   },
-
+  attached: function () {
+    this.getData();
+    // this.setData({
+    //   realFields: this.data.fields
+    // });
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    //获取数据
+    getData:function() {
+      const t = this;
+      if (t.properties.fieldsDs) {
+        wx._TY_Tool.getDSData(t.properties.fieldsDs,wx. _TY_Tool.buildTplParams(t), function (data) {
+          data.forEach((item) => {
+            const { dataKey, value } = item;
+            t.setData({
+              realFields:value
+            })
+          });
+        }, function (code, msg) {
+        });
+      }
+    },
+    //值改变
+    change:function(e) {
+      this.triggerEvent("change", e.detail.value);
+      // this.setData({
+      //   valueBase: e.default.value
+      // })
+    },
   }
 })
