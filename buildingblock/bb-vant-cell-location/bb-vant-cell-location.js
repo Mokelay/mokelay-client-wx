e4da581c6cfd3bf1c0f113b45bb9633a159099e8
xiaobc <527710279@qq.com>
2018-09-09 13:41:23 +0800

    location组件 init

---
+++ buildingblock/bb-vant-cell-location/bb-vant-cell-location.js
@@ -0,0 +1,30 @@
+// buildingblock/bb-vant-cell-location/bb-vant-cell-location.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    cellOpt:{
+      icon: "ty-wj_mall_address_ques",
+      title: "所在位置",
+      label: "",
+      required: false,
+      isLink: true,
+      center: true
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
