9b100b9c4b5d15ce621b269adb3607ccd975b6b5
xiaobc <527710279@qq.com>
2018-09-16 20:53:28 +0800

    build 巴斯

--- package/buzz/xy_partner_my_load.js
+++ package/buzz/xy_partner_my_load.js
@@ -1,10 +1,9 @@
 
   module.exports = function (...args) {
-    const app = getApp();
+ const app = getApp();
     const realDataMap = args[0].realDataMap;
  
   app.globalData._TY_Tool.findBBByUuid('DC153712-8BC6-48B5-BE74-969CE47364F7').setValue(realDataMap.data.mobilephone||'');
-  debugger;
 	return realDataMap.data;
 
 
