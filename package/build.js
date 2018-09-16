f73e5412f65bf7c064cdf3b5fd72d6bbad6baaab
陈征 <carl.chen@chinaredstar.com>
2018-09-16 20:06:43 +0800

    同步page配置到本地

--- package/build.js
+++ package/build.js
@@ -1,18 +1,15 @@
 //批处理脚本，用于从服务端下载所有的page配置以及巴斯函数
-
 var http = require('http');
 var querystring=require('querystring');
 var fs = require('fs');
 var path = require('path');
- 
-// var postData=querystring.stringify({
-// 	appAlias:'xy_partner'
-// });
- 
+var appAlias = 'xy_partner';
+
+//同步巴斯脚本
 var options = {
     host: 'www.mokelay.com',
     port: 80,
-    path: '/config/load-app-buzz?appAlias=xy_partner',
+    path: '/config/load-app-buzz?appAlias='+appAlias,
     method: 'GET',
     headers:{
         'accept': '*/*',
@@ -22,7 +19,6 @@ var options = {
         'user-agent': 'nodejs rest client'
     }
 };
- 
 var req = http.request(options, function (res) {
 	res.setEncoding('utf-8');
 	var content = [];
@@ -38,7 +34,7 @@ var req = http.request(options, function (res) {
     			var buzz = buzzList[i];
     			var alias = buzz['alias'];
     			var code = buzz['code'];
-    			console.log("Write:"+alias+".js");
+    			console.log("Write buzz:"+alias+".js");
 
     			var  p =path.join(__dirname,'buzz',alias+".js");
     			fs.writeFile(p,code,(err)=>{
@@ -48,15 +44,56 @@ var req = http.request(options, function (res) {
     				}
     			})
     		}
-    		// console.log(d);
-    		// 
-    		// console.log(p);
     	}
     });
 });
- 
 req.on('error', function(e) {
   console.log('problem with request: ' + e.message);
 });
- 
 req.end();
+
+//同步页面配置
+var options2 = {
+    host: 'www.mokelay.com',
+    port: 80,
+    path: '/config/load_app_page?appAlias='+appAlias,
+    method: 'GET',
+    headers:{
+        'accept': '*/*',
+        'content-type': "application/x-www-form-urlencoded",
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'user-agent': 'nodejs rest client'
+    }
+};
+var req2 = http.request(options2, function (res2) {
+    res2.setEncoding('utf-8');
+    var content2 = [];
+    res2.on('data',function (chunk) {
+        content2.push(chunk.toString());
+    });
+
+    res2.on('end',function(){
+        var d = JSON.parse(content2.join(''));
+        if(d['data'] && d['data']['data_list']){
+            var pageList = d['data']['data_list']['list'];
+            for(var i in pageList){
+                var page = pageList[i];
+                console.log("Write page:"+page['alias']+".js");
+
+                var  p =path.join(__dirname,'page',page['alias']+".js");
+                var code = "module.exports = "+JSON.stringify(page);
+                fs.writeFile(p,code,(err)=>{
+                    if(err){
+                        console.log("Write page error:");
+                        console.log(err);
+                    }
+                })
+            }
+        }
+    });
+});
+req2.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+req2.end();
