43e6b81e0a61c8f00ce8df589d64c2c38b7d7c94
usagitutu <usagitutu@192.168.1.3>
2018-09-17 00:16:49 +0800

    add buzz

---
+++ package/buzz/generate_xy_partner_news_detail_JiKlP60S.js
@@ -0,0 +1,14 @@
+
+  module.exports = function (...args) {
+//debugger;
+
+//return data.data.data;
+
+
+var fun = function(){
+  _TY_Tool.findBBByUuid('B0C5870C-354E-48AA-A5E5-B3C4449E3194').$el.src=realDataMap.data.cover;
+  _TY_Tool.findBBByUuid('3BAD14B0-C341-489E-AF94-53B3C2E2559F').content=realDataMap.data.createTime;
+	return realDataMap.data;
+}
+fun();
+  }
