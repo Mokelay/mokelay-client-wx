643969c241a51aa7cd3efd98639e678674616651
xuechao.zhang <xuechao.zhang@example.com>
2018-09-26 00:45:22 +0800

    env

--- env/index.js
+++ env/index.js
@@ -1,13 +1,13 @@
 //常量配置
 module.exports = {
 	name: 'local',
-	apiHost: "https://www.mokelay.com",
+  apiHost: "https://social-crm.xiaoyi.com",
 	h5Host: "",
   ssoURL: "index?alias=xy_partner_wx_auth",
 	hosts: {
-		'default': "https://www.mokelay.com"
+		'default': "https://social-crm.xiaoyi.com"
 	},
 	package:true,
-	questionnaireHost: "https://www.mokelay.com/survey",
+	questionnaireHost: "https://social-crm.xiaoyi.com/survey",
 	home: "xy_partner_home"
 };
